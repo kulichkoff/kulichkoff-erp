25c4e62a42974d52c86a02ea050a1aca503b7bd5
kulichkoff <kulichkov@background.digital>
2022-08-18 15:12:55 +0300

    MAIN: russian translate

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,16 +1,65 @@
-import { Component } from '@angular/core';
-import { Message } from '@app/api-interfaces';
-import { HttpClient } from '@angular/common/http';
+import {
+    Component,
+    OnInit,
+} from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-    hello$ = this.http.get<Message>('/api/hello');
+export class AppComponent implements OnInit {
 
     constructor(
-        private http: HttpClient,
+        private primeConfig: PrimeNGConfig,
     ) {}
+
+    public ngOnInit() {
+        this.primeConfig.setTranslation({
+            monthNames: [
+                'Январь',
+                'Февраль',
+                'Март',
+                'Апрель',
+                'Май',
+                'Июнь',
+                'Июль',
+                'Август',
+                'Сентябрь',
+                'Октябрь',
+                'Ноябрь',
+                'Декабрь',
+            ],
+            monthNamesShort: [
+                'Янв',
+                'Фев',
+                'Мар',
+                'Апр',
+                'Май',
+                'Июн',
+                'Июл',
+                'Авг',
+                'Сен',
+                'Окт',
+                'Ноя',
+                'Дек',
+            ],
+            dayNames: [
+                'Воскресенье',
+                'Понедельник',
+                'Вторник',
+                'Среда',
+                'Четверг',
+                'Пятница',
+                'Суббота',
+            ],
+            dayNamesShort: ['Воск', 'Пон', 'Вт', 'Ср', 'Четв', 'Пят', 'Суб'],
+            dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+            weekHeader: 'Неделя',
+            firstDayOfWeek: 1,
+            dateFormat: 'dd.mm.yy',
+        });
+    }
+
 }
